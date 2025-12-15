12ed347d3048950da8b95a0332ac9724aeeb0cc6
Moritz Eckert <MoritzEckert@web.de>
2025-12-15 15:39:48 +0100

    Remove unnecessary cpp code

--- cpp/include/matching/templateinstantiation.h
+++ cpp/include/matching/templateinstantiation.h
@@ -1,28 +1,17 @@
 #ifndef MATCHINGTEMPLATEINSTANTIATION_H
 #define MATCHINGTEMPLATEINSTANTIATION_H
 
-#include <swisssystems/burstein.h>
-#include <swisssystems/dutch.h>
+#include <matching/computer.h>
+#include <utility/dynamicuint.h>
 
-#ifdef OMIT_BURSTEIN
-#define MATCHING_EDGE_WEIGHT_BURSTEIN(a)
-#else
-#define MATCHING_EDGE_WEIGHT_BURSTEIN(a) \
-a(swisssystems::burstein::matching_computer::edge_weight)
-#endif
-
-#ifdef OMIT_DUTCH
-#define MATCHING_EDGE_WEIGHTS_DUTCH(a)
-#else
-#define MATCHING_EDGE_WEIGHTS_DUTCH(a) \
-a(swisssystems::dutch::validity_matching_computer::edge_weight) \
-a(swisssystems::dutch::optimality_matching_computer::edge_weight)
-#endif
+using validity_edge_weight = matching::computer_supporting_value<1>::type::edge_weight;
+using optimality_edge_weight = utility::uinttypes::DynamicUint;
 
 // This macro is called in the cpp files of the matching code to instantiate the
 // templates needed for the Swiss systems.
 
 #define INSTANTIATE_MATCHING_EDGE_WEIGHT_TEMPLATES(a) \
-MATCHING_EDGE_WEIGHT_BURSTEIN(a) MATCHING_EDGE_WEIGHTS_DUTCH(a)
+    a(validity_edge_weight) \
+    a(optimality_edge_weight)
 
 #endif
--- python_bindings/computer_bindings.cpp
+++ python_bindings/computer_bindings.cpp
@@ -2,48 +2,31 @@
 #include <pybind11/stl.h>
 #include <pybind11/numpy.h>
 #include "matching/computer.h"
-
-#ifndef OMIT_DUTCH
-#include <swisssystems/dutch.h>
-#endif
-
-#ifndef OMIT_BURSTEIN
-#include <swisssystems/burstein.h>
-#endif
+#include <utility/dynamicuint.h>
 
 namespace py = pybind11;
+
+using validity_edge_weight = matching::computer_supporting_value<1>::type::edge_weight;
+using optimality_edge_weight = utility::uinttypes::DynamicUint;
 
 PYBIND11_MODULE(matching_computer, m) {
     m.doc() = "Matching computer python bindings";
 
-#ifndef OMIT_BURSTEIN
-    py::class_<matching::Computer<swisssystems::burstein::matching_computer::edge_weight>>(m, "ComputerBurstein")
-        .def(py::init<typename matching::Computer<swisssystems::burstein::matching_computer::edge_weight>::size_type,
-                      const swisssystems::burstein::matching_computer::edge_weight&>())
-        .def("size", &matching::Computer<swisssystems::burstein::matching_computer::edge_weight>::size)
-        .def("add_vertex", &matching::Computer<swisssystems::burstein::matching_computer::edge_weight>::addVertex)
-        .def("set_edge_weight", &matching::Computer<swisssystems::burstein::matching_computer::edge_weight>::setEdgeWeight)
-        .def("compute_matching", &matching::Computer<swisssystems::burstein::matching_computer::edge_weight>::computeMatching)
-        .def("get_matching", &matching::Computer<swisssystems::burstein::matching_computer::edge_weight>::getMatching);
-#endif
+    py::class_<matching::Computer<validity_edge_weight>>(m, "ComputerDutchValidity")
+        .def(py::init<typename matching::Computer<validity_edge_weight>::size_type,
+                      const validity_edge_weight&>())
+        .def("size", &matching::Computer<validity_edge_weight>::size)
+        .def("add_vertex", &matching::Computer<validity_edge_weight>::addVertex)
+        .def("set_edge_weight", &matching::Computer<validity_edge_weight>::setEdgeWeight)
+        .def("compute_matching", &matching::Computer<validity_edge_weight>::computeMatching)
+        .def("get_matching", &matching::Computer<validity_edge_weight>::getMatching);
 
-#ifndef OMIT_DUTCH
-    py::class_<matching::Computer<swisssystems::dutch::validity_matching_computer::edge_weight>>(m, "ComputerDutchValidity")
-        .def(py::init<typename matching::Computer<swisssystems::dutch::validity_matching_computer::edge_weight>::size_type,
-                      const swisssystems::dutch::validity_matching_computer::edge_weight&>())
-        .def("size", &matching::Computer<swisssystems::dutch::validity_matching_computer::edge_weight>::size)
-        .def("add_vertex", &matching::Computer<swisssystems::dutch::validity_matching_computer::edge_weight>::addVertex)
-        .def("set_edge_weight", &matching::Computer<swisssystems::dutch::validity_matching_computer::edge_weight>::setEdgeWeight)
-        .def("compute_matching", &matching::Computer<swisssystems::dutch::validity_matching_computer::edge_weight>::computeMatching)
-        .def("get_matching", &matching::Computer<swisssystems::dutch::validity_matching_computer::edge_weight>::getMatching);
-
-    py::class_<matching::Computer<swisssystems::dutch::optimality_matching_computer::edge_weight>>(m, "ComputerDutchOptimality")
-        .def(py::init<typename matching::Computer<swisssystems::dutch::optimality_matching_computer::edge_weight>::size_type,
-                      const swisssystems::dutch::optimality_matching_computer::edge_weight&>())
-        .def("size", &matching::Computer<swisssystems::dutch::optimality_matching_computer::edge_weight>::size)
-        .def("add_vertex", &matching::Computer<swisssystems::dutch::optimality_matching_computer::edge_weight>::addVertex)
-        .def("set_edge_weight", &matching::Computer<swisssystems::dutch::optimality_matching_computer::edge_weight>::setEdgeWeight)
-        .def("compute_matching", &matching::Computer<swisssystems::dutch::optimality_matching_computer::edge_weight>::computeMatching)
-        .def("get_matching", &matching::Computer<swisssystems::dutch::optimality_matching_computer::edge_weight>::getMatching);
-#endif
+    py::class_<matching::Computer<optimality_edge_weight>>(m, "ComputerDutchOptimality")
+        .def(py::init<typename matching::Computer<optimality_edge_weight>::size_type,
+                      const optimality_edge_weight&>())
+        .def("size", &matching::Computer<optimality_edge_weight>::size)
+        .def("add_vertex", &matching::Computer<optimality_edge_weight>::addVertex)
+        .def("set_edge_weight", &matching::Computer<optimality_edge_weight>::setEdgeWeight)
+        .def("compute_matching", &matching::Computer<optimality_edge_weight>::computeMatching)
+        .def("get_matching", &matching::Computer<optimality_edge_weight>::getMatching);
 }
